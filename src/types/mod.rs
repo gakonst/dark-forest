0c6a56e0c3251604a6d4e1256639185c89325cb1
Georgios Konstantopoulos <me@gakonst.com>
2021-07-01 00:57:55 +0300

    feat: add simple main file for querying planets

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,8 +1,6 @@
 //! Dark Forest-related types
-use derive_more::AsRef;
-use ethers::types::U256;
-use serde::{de, Deserialize, Serialize};
-
 pub mod map;
+pub use map::Map;
+
 pub mod planet;
 // TODO: Add artifacts module
