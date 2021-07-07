b36f4f37d621193d0cf54ffe92e1731b4c049b10
Georgios Konstantopoulos <me@gakonst.com>
2021-07-08 00:03:19 +0300

    feat: helpers for converting between ark <> ethers types

--- crates/ark-circom/src/solidity_compat/types.rs
+++ crates/ark-circom/src/solidity_compat/types.rs
@@ -1,7 +1,7 @@
-use ark_ff::{BigInteger, PrimeField};
+use ark_ff::{BigInteger, FromBytes, PrimeField};
 use ethers::types::U256;
 
-use ark_bn254::{Bn254, Fr, G1Affine, G2Affine};
+use ark_bn254::{Bn254, Fq2, Fr, G1Affine, G2Affine};
 
 pub struct Inputs(pub Vec<U256>);
 
@@ -20,10 +20,18 @@ impl From<&[Fr]> for Inputs {
     }
 }
 
-#[derive(Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct G1 {
     pub x: U256,
     pub y: U256,
+}
+
+impl From<G1> for G1Affine {
+    fn from(src: G1) -> G1Affine {
+        let x = u256_to_point(src.x);
+        let y = u256_to_point(src.y);
+        G1Affine::new(x, y, false)
+    }
 }
 
 type G1Tup = (U256, U256);
@@ -43,10 +51,24 @@ impl From<&G1Affine> for G1 {
     }
 }
 
-#[derive(Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct G2 {
     pub x: [U256; 2],
     pub y: [U256; 2],
+}
+
+impl From<G2> for G2Affine {
+    fn from(src: G2) -> G2Affine {
+        let c0 = u256_to_point(src.x[0]);
+        let c1 = u256_to_point(src.x[1]);
+        let x = Fq2::new(c0, c1);
+
+        let c0 = u256_to_point(src.y[0]);
+        let c1 = u256_to_point(src.y[1]);
+        let y = Fq2::new(c0, c1);
+
+        G2Affine::new(x, y, false)
+    }
 }
 
 type G2Tup = ([U256; 2], [U256; 2]);
@@ -66,7 +88,7 @@ impl From<&G2Affine> for G2 {
     }
 }
 
-#[derive(Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Proof {
     a: G1,
     b: G2,
@@ -85,6 +107,16 @@ impl From<ark_groth16::Proof<Bn254>> for Proof {
             a: G1::from(&proof.a),
             b: G2::from(&proof.b),
             c: G1::from(&proof.c),
+        }
+    }
+}
+
+impl From<Proof> for ark_groth16::Proof<Bn254> {
+    fn from(src: Proof) -> ark_groth16::Proof<Bn254> {
+        ark_groth16::Proof {
+            a: src.a.into(),
+            b: src.b.into(),
+            c: src.c.into(),
         }
     }
 }
@@ -122,9 +154,122 @@ impl From<ark_groth16::VerifyingKey<Bn254>> for VerifyingKey {
     }
 }
 
+impl From<VerifyingKey> for ark_groth16::VerifyingKey<Bn254> {
+    fn from(src: VerifyingKey) -> ark_groth16::VerifyingKey<Bn254> {
+        ark_groth16::VerifyingKey {
+            alpha_g1: src.alpha1.into(),
+            beta_g2: src.beta2.into(),
+            gamma_g2: src.gamma2.into(),
+            delta_g2: src.delta2.into(),
+            gamma_abc_g1: src.ic.into_iter().map(Into::into).collect(),
+        }
+    }
+}
+
 // Helper for converting a PrimeField to its U256 representation for Ethereum compatibility
+fn u256_to_point<F: PrimeField>(point: U256) -> F {
+    let mut buf = [0; 32];
+    point.to_little_endian(&mut buf);
+    let bigint = F::BigInt::read(&buf[..]).expect("always works");
+    F::from_repr(bigint).expect("alwasy works")
+}
+
+// Helper for converting a PrimeField to its U256 representation for Ethereum compatibility
+// (U256 reads data as big endian)
 fn point_to_u256<F: PrimeField>(point: F) -> U256 {
     let point = point.into_repr();
-    let point_bytes = point.to_bytes_le();
+    let point_bytes = point.to_bytes_be();
     U256::from(&point_bytes[..])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_bn254::Fq;
+
+    fn fq() -> Fq {
+        Fq::from(2)
+    }
+
+    fn fq2() -> Fq2 {
+        Fq2::from(2)
+    }
+
+    fn fr() -> Fr {
+        Fr::from(2)
+    }
+
+    fn g1() -> G1Affine {
+        G1Affine::new(fq(), fq(), false)
+    }
+
+    fn g2() -> G2Affine {
+        G2Affine::new(fq2(), fq2(), false)
+    }
+
+    #[test]
+    fn convert_fq() {
+        let el = fq();
+        let el2 = point_to_u256(el);
+        let el3: Fq = u256_to_point(el2);
+        let el4 = point_to_u256(el3);
+        assert_eq!(el, el3);
+        assert_eq!(el2, el4);
+    }
+
+    #[test]
+    fn convert_fr() {
+        let el = fr();
+        let el2 = point_to_u256(el);
+        let el3: Fr = u256_to_point(el2);
+        let el4 = point_to_u256(el3);
+        assert_eq!(el, el3);
+        assert_eq!(el2, el4);
+    }
+
+    #[test]
+    fn convert_g1() {
+        let el = g1();
+        let el2 = G1::from(&el);
+        let el3: G1Affine = el2.into();
+        let el4 = G1::from(&el3);
+        assert_eq!(el, el3);
+        assert_eq!(el2, el4);
+    }
+
+    #[test]
+    fn convert_g2() {
+        let el = g2();
+        let el2 = G2::from(&el);
+        let el3: G2Affine = el2.into();
+        let el4 = G2::from(&el3);
+        assert_eq!(el, el3);
+        assert_eq!(el2, el4);
+    }
+
+    #[test]
+    fn convert_vk() {
+        let vk = ark_groth16::VerifyingKey::<Bn254> {
+            alpha_g1: g1(),
+            beta_g2: g2(),
+            gamma_g2: g2(),
+            delta_g2: g2(),
+            gamma_abc_g1: vec![g1(), g1(), g1()],
+        };
+        let vk_ethers = VerifyingKey::from(vk.clone());
+        let ark_vk: ark_groth16::VerifyingKey<Bn254> = vk_ethers.into();
+        assert_eq!(ark_vk, vk);
+    }
+
+    #[test]
+    fn convert_proof() {
+        let p = ark_groth16::Proof::<Bn254> {
+            a: g1(),
+            b: g2(),
+            c: g1(),
+        };
+        let p2 = Proof::from(p.clone());
+        let p3 = ark_groth16::Proof::from(p2);
+        assert_eq!(p, p3);
+    }
 }
